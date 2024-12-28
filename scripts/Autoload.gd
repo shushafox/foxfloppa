d2b9da388eb8a65d59d831570cf69d1ecc5ef0db
shushafox <callmeshushio@gmail.com>
2024-12-28 23:19:10 +0300

    Расширил карту, добавил второго нпс
    Теперь на карте ещё есть Корви, её диалог зависит от того, говорил ты с абобусом или нет

--- scripts/Autoload.gd
+++ scripts/Autoload.gd
@@ -2,10 +2,12 @@ extends Node
 
 var GlobalCounter:int = 1
 var IsDay: bool = true
+var EvilTalkedTo: bool = false
 
 func NpcTimeLineResolver(name: String) -> String:
 	match name:
 		"evil": return _EvilGuy()
+		"corvi": return _Corvi()
 		_: return "" # defaut
 
 func _EvilGuy() -> String:
@@ -13,3 +15,10 @@ func _EvilGuy() -> String:
 		return "angi"
 	else:
 		return "testOne"	
+
+func _Corvi() -> String:
+	if (EvilTalkedTo == true):
+		return "corvi_2"
+	else:
+		return "corvi_1"
+		
